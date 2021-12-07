f84c50876307206a7559f3a4755231e86c2a892f
atenorio3 <68285822+atenorio3@users.noreply.github.com>
2021-12-07 14:38:36 -0600

    completedDemo1

--- Assets/Scripts/Camera_Control.cs
+++ Assets/Scripts/Camera_Control.cs
@@ -4,14 +4,59 @@ using UnityEngine;
 
 public class Camera_Control : MonoBehaviour
 {
-     public float CAMERASPEED = 0.05f;
+     private float MOVESPEED = 5f;
+     private float ROTATIONSPEED = 3f;
+     private float yaw = 0f;
+     private float pitch = 0f;
 
-    // Update is called once per frame
-    void Update()
+     private void Start()
+     {
+          Cursor.visible = false;
+     }
+     // Update is called once per frame
+     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+          // Snap Camera back to center
+          if (Input.GetKey(KeyCode.Space))
           {
-               transform.position += CAMERASPEED * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * Time.deltaTime;
+               transform.position = new Vector3(0f, 1f, -10f);
+               yaw = 0f;
+               pitch = 0f;
+               transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+          }
+          // WASD Controls
+          if (Input.GetKey(KeyCode.W))
+          {
+               transform.position += transform.forward * MOVESPEED * Time.deltaTime;
+          }
+          if (Input.GetKey(KeyCode.A))
+          {
+               transform.position -= transform.right * MOVESPEED * Time.deltaTime;
+          }
+          if (Input.GetKey(KeyCode.S))
+          {
+               transform.position -= transform.forward * MOVESPEED * Time.deltaTime;
+          }
+          if (Input.GetKey(KeyCode.D))
+          {
+               transform.position += transform.right * MOVESPEED * Time.deltaTime;
+          }
+          if (Input.GetKey(KeyCode.Q))
+          {
+               transform.position -= transform.up * MOVESPEED * Time.deltaTime;
+          }
+          if (Input.GetKey(KeyCode.E))
+          {
+               transform.position += transform.up * MOVESPEED * Time.deltaTime;
+          }
+
+          // Camera Rotation
+          if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+          {
+               yaw += ROTATIONSPEED * Input.GetAxis("Mouse X");
+               pitch -= ROTATIONSPEED * Input.GetAxis("Mouse Y");
+
+               transform.eulerAngles = new Vector3(pitch, yaw, 0f);
           }
     }
 }
